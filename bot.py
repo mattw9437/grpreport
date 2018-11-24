2e717974c363d14c24aef4967ce11b40fb544e76
vdsk <vsk@duseks-iMac.local>
2018-11-24 21:49:40 -0200

    added id checking

--- bot.py
+++ bot.py
@@ -13,6 +13,16 @@ list = os.getenv("listid")
 
 bot = commands.Bot(command_prefix="!")
 
+def grabId(rblxuser):
+    apiresp = requests.get(rblxapi, timeout=5, data={"username" : rblxuser})
+    pijson = json.loads(apiresp.text)
+
+    if "Id" in pijson:
+        return (pijson["Id"])
+
+    else:
+        return False
+
 @bot.event
 async def on_ready():
 	print ("meme test")
@@ -21,7 +31,7 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def r(ctx, user, proof, proof2 = "", proof3 = ""):
-	if ctx.message.channel.id == os.getenv("channelid"):
+	if ctx.message.channel.id == os.getenv("channelid") and grabId(user) != False:
 		author = ctx.message.author		
 		adesc = "{0} \n {1} \n {2} \n Report by: {3} \n DiscordID: {4}".format(proof ,proof2, proof3, author, author.id)
 		cdata = {"key":trellokey,"token":token,"idList":list,"name":user,"desc":adesc}
@@ -29,6 +39,9 @@ async def r(ctx, user, proof, proof2 = "", proof3 = ""):
 		jsont = json.loads(req.text)
 		clink = jsont["shortUrl"]
 		await bot.say ("ty" + author.mention + " " + clink)
+	elif ctx.message.channel.id == os.getenv("channelid") and grabId(user) == False:
+		await bot.say ("{0} is not a valid username {1}".format(user, author.mention))
+		
 
 
 @bot.command(pass_context=True)
