9fabb3d01738ef33818601c1fe9c138b48b561ff
vdsk <45274570+vdsk@users.noreply.github.com>
2018-11-24 11:34:50 -0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def r(ctx, user, proof, proof2 = "", proof3 = ""):
 
 
 @bot.command(pass_context=True)
-async def help(ctx):
+async def h(ctx):
 if ctx.message.channel.id == os.getenv("channelid"):
 	await bot.say ("to report use !r useryouarereporting proof1 proof2 proof3, you can add up to 3 proofs ")
 
