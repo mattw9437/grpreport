ebde1fb6b1a6b738c034d3723d3c7355797c0609
vdsk <vsk@duseks-iMac.local>
2018-11-25 20:15:28 -0200

    added infinite proof possiblity and username id adder

--- bot.py
+++ bot.py
@@ -37,16 +37,20 @@ async def h(ctx):
 		await bot.say ("to report use !r useryouarereporting proof1 proof2 proof3, you can add up to 3 proofs ")
 
 @bot.command(pass_context=True)
-async def r(ctx, user, proof, proof2 = "", proof3 = ""):
-	author = ctx.message.author	
-	if ctx.message.channel.id == os.getenv("channelid") and grabId(user) != False:	
-		adesc = "{0} \n {1} \n {2} \n Report by: {3} \n DiscordID: {4}".format(proof ,proof2, proof3, author, author.id)
-		cdata = {"key":trellokey,"token":token,"idList":list,"name":user,"desc":adesc}
+async def r(ctx, user, proof, *proofarg):
+	author = ctx.message.author
+	userid = grabId(user)	
+	if ctx.message.channel.id == os.getenv("channelid") and userid != False:
+		descproof = ""	
+		for proofs in proofarg:
+			descproof += "{} \n".format(proofs)
+		adesc = "{1} \n Report by: {2} \n DiscordID: {3}".format(descproof, author, author.id)
+		cdata = {"key":trellokey,"token":token,"idList":list,"name":user + ":" + userid,"desc":adesc}
 		req = requests.post(boardurl, params=cdata)
 		jsont = json.loads(req.text)
 		clink = jsont["shortUrl"]
 		await bot.say ("ty" + author.mention + " " + clink)
-	elif ctx.message.channel.id == os.getenv("channelid") and grabId(user) == False:
+	elif ctx.message.channel.id == os.getenv("channelid") and userid == False:
 		await bot.say ("{0} is not a valid username {1}".format(user, author.mention))
 		
 
