70b7f65b10689e184fce13f2bae8fbffcd22def4
vdsk <vsk@duseks-iMac.local>
2018-11-25 20:49:02 -0200

    scuffed python2

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def r(ctx, user, proof, *args):
 		descproof = ""	
 		for proofs in args:
 			descproof += proofs + "\n"
-		adesc = proofs +  "\n Report by: {1} \n DiscordID: {2}".format(author, author.id)
+		adesc = proofs +  "\n Report by:" + author +  "\n DiscordID:" + author.id
 		cdata = {"key":trellokey,"token":token,"idList":list,"name":user + ":" + userid,"desc":adesc}
 		req = requests.post(boardurl, params=cdata)
 		jsont = json.loads(req.text)
