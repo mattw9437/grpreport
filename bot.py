b4b0415c42d37438a372de77b23e234cde7e25c1
vdsk <vsk@duseks-iMac.local>
2018-11-25 20:17:31 -0200

    fixed stupid mistake

--- bot.py
+++ bot.py
@@ -37,12 +37,12 @@ async def h(ctx):
 		await bot.say ("to report use !r useryouarereporting proof1 proof2 proof3, you can add up to 3 proofs ")
 
 @bot.command(pass_context=True)
-async def r(ctx, user, proof, *proofarg):
+async def r(ctx, user, proof, *args):
 	author = ctx.message.author
 	userid = grabId(user)	
 	if ctx.message.channel.id == os.getenv("channelid") and userid != False:
 		descproof = ""	
-		for proofs in proofarg:
+		for proofs in args:
 			descproof += "{} \n".format(proofs)
 		adesc = "{1} \n Report by: {2} \n DiscordID: {3}".format(descproof, author, author.id)
 		cdata = {"key":trellokey,"token":token,"idList":list,"name":user + ":" + userid,"desc":adesc}
